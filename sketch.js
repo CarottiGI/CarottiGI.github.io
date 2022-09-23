b0c49402e808869981ebd4b2230f7777ee3307fe
CarottiGI <113664315+CarottiGI@users.noreply.github.com>
2022-09-23 08:35:36 +0200

    Add bouncing ball

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
 function setup() {
-  createCanvas(400, 400);
+  limitx = 600;
+  limity = 400;
+  createCanvas(limitx, limity);
+  speed = 15;
+  x = 50;
+  vx = speed;
+  y = 50;
+  vy = speed;
+  d = 30;
+  switchbg = 0;
 }
 
 function draw() {
-  background(220);
+  if (switchbg == 0) {
+    background(75);
+  } else {
+    background(255);
+  }
+  if (x >= limitx-(d/2)) {
+    vx = -1*vx;
+  } if (x <= 0+(d/2)) {
+    vx = -1*vx;
+  } 
+  
+  if (y >= limity-(d/2)) {
+    vy = -1*vy;
+  } if (y < 15) {
+    vy = -1*vy;
+  }
+  x = x+vx
+  y = y+vy
+  fill(255, 150, 100);
+  circle(x, y, d);
 }
